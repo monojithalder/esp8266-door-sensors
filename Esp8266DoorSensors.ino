c3f0aff86ae5e848fc87e177e2f5c6cbd1be8ec1
monojithalder <monojithalder@hotmail.com>
2018-08-12 23:19:35 +0530

    Remove Software serial and add do-login,myaccoount,on-buzzer,off-buzzer,logout funcationality

--- Esp8266DoorSensors.ino
+++ Esp8266DoorSensors.ino
@@ -6,7 +6,7 @@
 #include <EEPROM.h>
 #include<SoftwareSerial.h>
 
-SoftwareSerial s(2,3);
+//SoftwareSerial s(2,3);
 ESP8266WebServer server(80);
 
 const char* ssid     = "WEBTECHLINK";
@@ -17,7 +17,8 @@ IPAddress gateway(192,168,1,1); // set gateway to match your network
 IPAddress subnet(255, 255, 255, 0);
 int receiveddata =0;
 int password_address = 20;
-
+int login = 0;
+int buzzer_address = 30;
 void setup() {
     EEPROM.begin(512);
   //eeWriteInt(2,30);
@@ -26,13 +27,20 @@ void setup() {
     eeWriteInt(i,0);
   }
   eeWriteInt(password_address,-1);
-  
+  pinMode(2,OUTPUT);
+  digitalWrite(2,HIGH);  
   WiFi.mode(WIFI_AP);
   //WiFi.softAPConfig(esp_ip, esp_ip, subnet);        // declared as: bool softAPConfig (IPAddress local_ip, IPAddress gateway, IPAddress subnet)
-  WiFi.softAP("Ayan_Door_Sensor", "password", 7);
+  WiFi.softAP("TESTING", "password", 7);
   
   server.on("/",indexPage);
-  server.on("change-password",changePassword);
+  server.on("/change-password",changePassword);
+  server.on("/do-change-password",doChangePassword);
+  server.on("/do-login",doLogin);
+  server.on("/myaccount",myAccount);
+  server.on("/on-buzzer",onBuzzer);
+  server.on("/off-buzzer",offBuzzer);
+  server.on("/logout",logout);
   server.begin();
   delay(500);
   
@@ -41,17 +49,17 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-  s.begin(9600);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  server.handleClient();
 
 }
 
 void indexPage() {
-  if(EEPROM.read(password_address) == -1) {
+  digitalWrite(2,HIGH);
+  if(eeGetInt(password_address) == -1) {
     server.sendHeader("Location", "change-password", true);
     server.send ( 302, "text/plain", "");
   }
@@ -59,7 +67,7 @@ void indexPage() {
     String html;
     html = "<html><head><title>Door Sensors</title></head><body>";
     html += "<div style=\"width:100%;margin-right:auto;margin-left:auto;\">";
-    html += "<form action=\"/myaccount\" method=\"post\">";
+    html += "<form action=\"/do-login\" method=\"post\">";
     html += "<input type=\"password\" name=\"password\">";
     html += "<input type=\"submit\" value=\"Enter\">";
     html += "<div></form>";
@@ -72,10 +80,11 @@ void indexPage() {
 }
 
 void changePassword() {
+  digitalWrite(2,LOW);
   String html;
   html = "<html><head><title>Door Sensors | Change Password</title></head><body>";
   html += "<div style=\"width:100%;margin-right:auto;margin-left:auto;\">";
-  html += "<form action=\"/myaccount\" method=\"post\">";
+  html += "<form action=\"/do-change-password\" method=\"post\">";
   html += "<input type=\"password\" name=\"password\">";
   html += "<input type=\"submit\" value=\"Enter\">";
   html += "<div></form>";
@@ -84,6 +93,100 @@ void changePassword() {
   html += "<lable>Change Password "+ password +"</lable>";
   html += "</body></html>";
   server.send(200, "text/html", html);   
+}
+
+void doChangePassword() {
+  String password_string;
+  char password_char[100];
+  int password_int = 0;
+  if (server.arg("password")== ""){     //Parameter not found
+    server.sendHeader("Location", "change-password", true);
+    server.send ( 302, "text/plain", "");
+  }
+  else{ 
+    password_string = server.arg("password");
+    password_string.toCharArray(password_char,100);
+    password_int = atoi(password_char);
+    eeWriteInt(password_address,password_int);
+    server.sendHeader("Location", "/", true);
+    server.send ( 302, "text/plain", "");
+  }
+}
+
+void doLogin() {
+  String password_string;
+  char password_char[100];
+  int password_int = 0;
+  int store_password = 0;
+  if (server.arg("password")== ""){     //Parameter not found
+    server.sendHeader("Location", "/", true);
+    server.send ( 302, "text/plain", "");
+  }
+  else{ 
+    password_string = server.arg("password");
+    password_string.toCharArray(password_char,100);
+    password_int = atoi(password_char);
+    store_password = eeGetInt(password_address);
+    if(store_password == password_int) {
+      login = 1;
+      server.sendHeader("Location", "/myaccount", true);
+      server.send ( 302, "text/plain", ""); 
+    }
+    else {
+      server.sendHeader("Location", "/", true);
+      server.send ( 302, "text/plain", "");
+    }
+  }
+}
+
+void myAccount() {
+  if(login == 1) {
+    String html;
+    int buzzer_status = eeGetInt(buzzer_address);
+    html = "<html><head><title>Door Sensors | My Account</title></head>";
+    html += "<body><h1>This is My Account Page</h1>";
+    html += "<a href=\"/logout\">Logout</a>";
+    if(buzzer_status == 0) {
+      html += "<a href=\"/on-buzzer\">ON</a>";
+    }
+    else {
+      html += "<a href=\"/off-buzzer\">OFF</a>";
+    }
+    server.send(200, "text/html", html);
+  }
+  else {
+     server.sendHeader("Location", "/", true);
+     server.send ( 302, "text/plain", "");
+  }
+}
+
+void onBuzzer() {
+  if(login == 1) {
+    eeWriteInt(buzzer_address,1);
+    server.sendHeader("Location", "/myaccount", true);
+    server.send ( 302, "text/plain", "");
+  }
+  else {
+    server.sendHeader("Location", "/", true);
+    server.send ( 302, "text/plain", "");
+  }
+}
+
+void offBuzzer() {
+  if(login == 1) {
+    eeWriteInt(buzzer_address,0);
+    server.sendHeader("Location", "/myaccount", true);
+    server.send ( 302, "text/plain", "");
+  }
+  else {
+    server.sendHeader("Location", "/", true);
+    server.send ( 302, "text/plain", "");
+  }
+}
+void logout() {
+  login = 0;
+  server.sendHeader("Location", "/", true);
+  server.send ( 302, "text/plain", "");
 }
 
 void eeWriteInt(int pos, int val) {
